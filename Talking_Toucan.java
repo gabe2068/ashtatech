33d61746af83596ea660cab588da0e451fd790d1
gabe2068 <gaberogers2068@gmail.com>
2016-10-11 21:06:02 -0500

    Add files via upload

---
+++ Talking_Toucan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+public class Talking_Toucan {
+    public static void main(String[] args) {
+        System.out.println("Whats up?");
+        Scanner scan = new Scanner(System.in); 
+        scan.nextLine(); 
+        System.out.println("Thats cool, hows school going?"); 
+        scan.nextLine();
+        System.out.println("Thats good, and pretty good for me too, being a computer and all"); 
+
+    }
+}
